b3a7f75213f986601269e8c537921090c61ec601
Iyan345 <59591075+Iyan345@users.noreply.github.com>
2020-09-09 13:12:41 +0700

    Add files via upload

---
+++ substring.java
@@ -0,0 +1,16 @@
+package UTS;
+
+public class substring {
+    public static void main(String[] args){
+        String alfabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ#-";
+
+        System.out.println(alfabet);
+        System.out.println("" + alfabet.substring(18,19));
+        System.out.println("" + alfabet.substring(4,5));
+        System.out.println("" + alfabet.substring(15,16));
+        System.out.println("" + alfabet.substring(19,20));
+        System.out.println("" + alfabet.substring(8,9));
+        System.out.println("" + alfabet.substring(0,1));
+        System.out.println("" + alfabet.substring(13,14));
+    }
+}
